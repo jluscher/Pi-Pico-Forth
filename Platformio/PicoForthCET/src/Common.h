56431b6282340d4ced2ed7fce8904eaea77625dd
djw1wahl <dwahl@psl.wisc.edu>
2022-04-07 09:42:46 -0500

    Files for TheWizard to look at
    this is the project I am working on. at the moment the main() simply writes "hello world"
    I am trying to get the USB connected as stdio to work.
    dan

---
+++ Platformio/PicoForthCET/src/Common.h
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdint.h>
+#include "pico/stdlib.h"
+//
+// https://github.com/stm32duino/BoardManagerFiles/raw/main/package_stmicroelectronics_index.json
+//
+#define MEMSIZE 16384
+
